d5e41a0bc31ff47a26eafb60c3091a19338f10c7
Eri Schon <erischon@gmail.com>
2021-05-02 11:46:29 +0200

    tinymce installation and configuration

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+from tinymce.models import HTMLField
+
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -18,7 +20,8 @@ class Post(models.Model):
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
-    body = models.TextField()
+    body = HTMLField()
+    # body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
     # Blog
-    path('blog/', include('blog.urls', namespace='blog'))
+    path('blog/', include('blog.urls', namespace='blog')),
+    # Other
+    path('tinymce/', include('tinymce.urls')),
 ]
