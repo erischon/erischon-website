002af0b467010cb37470ab1a071a68987470bfce
Eri Schon <erischon@gmail.com>
2021-05-14 11:02:20 +0200

    tests optimisation

--- portfolio/tests/test_views.py
+++ portfolio/tests/test_views.py
@@ -1,13 +1,26 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 
+from portfolio.models import Project, Techno
+
 
 class PortfolioTestViews(TestCase):
 
     def setUp(self):
         self.client = Client()
 
+        self.techno = Techno('Django')
+        self.project = Project.objects.create(
+            title='project title',
+            description='project description',
+            detail='project detail',
+            image='image.jpg',
+            url_project='url',
+            url_git='url',
+        )
+
         self.portfolio_url = reverse('portfolio-hp')
+        self.project_url = reverse('project-detail', args=[self.project.id])
 
     def test_homepage_view(self):
         """ Test that """
@@ -15,3 +28,10 @@ class PortfolioTestViews(TestCase):
 
         self.assertEquals(response.status_code, 200)
         self.assertTemplateUsed(response, 'portfolio/index.html')
+
+    def test_project_page_view(self):
+        """ """
+        response = self.client.get(self.project_url, {'id': self.project.id})
+
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(response, 'portfolio/project.html')
--- portfolio/views.py
+++ portfolio/views.py
@@ -6,7 +6,7 @@ def portfolio_homepage(request):
     projects = Project.objects.all()
     return render(request, 'portfolio/index.html', {'projects': projects})
 
+
 def project(request, id):
     project = Project.objects.get(id=id)
     return render(request, 'portfolio/project.html', {'project': project})
-
