3f5debe2cca89ffc05f3d0379b5723678e2c6a54
Eri Schon <erischon@gmail.com>
2021-05-14 12:52:34 +0200

    working on form in the portfolio page

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .models import Project
+from website.forms import ContactForm
 
 
 def portfolio_homepage(request):
     projects = Project.objects.all()
-    return render(request, 'portfolio/index.html', {'projects': projects})
+    contact_form = ContactForm(request.POST) 
+    return render(request, 'portfolio/index.html', {'projects': projects, 'form': contact_form})
 
 
 def project(request, id):
